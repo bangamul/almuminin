6a45a609ee549be099e518e6dd613364c8547602
Ahmad Mulyana <ahmadmulyana0807@gmail.com>
2023-03-19 00:47:24 +0700

    update pdf

--- src/app/ratibhaddad/ratibhaddad.page.ts
+++ src/app/ratibhaddad/ratibhaddad.page.ts
@@ -4,8 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-ratibhaddad',
   templateUrl: './ratibhaddad.page.html',
   styleUrls: ['./ratibhaddad.page.scss'],
+  template: '<pdf-viewer [src]="pdfSrc" [render-text]="true" [original-size]="false" style="width: 400px; height: 500px"></pdf-viewer>',
 })
 export class RatibhaddadPage implements OnInit {
+
+  pdfSrc = "https://perspektiv.id/kitabpdf/ratibalhaddad.pdf";
 
   constructor() { }
 
--- src/app/risalatuljamiah/risalatuljamiah.page.ts
+++ src/app/risalatuljamiah/risalatuljamiah.page.ts
@@ -4,8 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-risalatuljamiah',
   templateUrl: './risalatuljamiah.page.html',
   styleUrls: ['./risalatuljamiah.page.scss'],
+  template: '<pdf-viewer [src]="pdfSrc" [render-text]="true" [original-size]="false" style="width: 400px; height: 500px"></pdf-viewer>',
 })
 export class RisalatuljamiahPage implements OnInit {
+
+  pdfSrc = "https://perspektiv.id/kitabpdf/risalatuljamiah.pdf";
 
   constructor() { }
 
