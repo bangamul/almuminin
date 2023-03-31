7603051d5e27ca8e155ba9c789de70df495d7665
Ahmad Mulyana <ahmadmulyana0807@gmail.com>
2023-04-01 01:57:50 +0700

    update style

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -15,6 +15,10 @@ export class GalleryPage implements OnInit {
   nama : any;
   viewimage: any = {};
   linkImage: any = {};
+  infohnya: any = {};
+  // gambarnya: any = {};
+  gambarnya : any[] = [];
+  gambarnyakecuali : any[] = [];
 
   constructor(
     public _apiServices: ApiService,
@@ -26,6 +30,8 @@ export class GalleryPage implements OnInit {
       this.id_unique = param.id_unique;
       console.log(this.id_unique);
       this.getgallerydetail(this.id_unique);
+      this.getGalleryinside();
+      this.getgalleryexcept(this.id_unique);
     })
   }
 
@@ -37,11 +43,31 @@ export class GalleryPage implements OnInit {
       console.log('okey', res);
       this.viewimage = JSON.parse(JSON.stringify(res));
       var filesnyacoba = this.viewimage.nama;
+      var infoh = this.viewimage.info;
       this.linkImage = 'https://perspektiv.id/musholla/gallery/' + filesnyacoba;
-      console.log('coba gambarnya', filesnyacoba);
+      this.infohnya = infoh;
+      console.log('coba info', this.infohnya);
     }, (error: any) => {
       console.log('error', error);
       alert('gagal ambil data');
+    })
+  }
+
+  getGalleryinside() {
+    this._apiServices.getGallery().subscribe((res:any)=>{
+      console.log('cek', res);
+      this.gambarnya = res;
+    },(error:any)=>{
+      console.log('error',error);
+    })
+  }
+
+  getgalleryexcept(id_unique) {
+    this._apiServices.getgalleryexcept(id_unique).subscribe((res:any)=>{
+      console.log('cek hasilnya ya', res);
+      this.gambarnyakecuali = res;
+    },(error:any)=>{
+      console.log('error',error);
     })
   }
 
